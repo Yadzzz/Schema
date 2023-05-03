ed045f16dab614e0942ff4757c9509185cdd700f
Yad <60503097+Yadzzz@users.noreply.github.com>
2023-05-03 21:19:20 +0200

    987897889987879897987

--- Schema/Services/BookingRequestsService.cs
+++ Schema/Services/BookingRequestsService.cs
@@ -1,4 +1,6 @@
-﻿namespace Schema.Services
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Schema.Services
 {
     public class BookingRequestsService
     {
@@ -7,6 +9,16 @@
         public BookingRequestsService(DataAccessLibrary.Context.BevakningContext bevakningContext)
         {
             this._bevakningContext = bevakningContext;
+        }
+
+        public async Task<List<DataAccessLibrary.Models.ScheduleRequests>?> GetBookingRequests()
+        {
+            if (this._bevakningContext == null || this._bevakningContext.ScheduleRequests == null)
+            {
+                return null;
+            }
+
+            return await this._bevakningContext.ScheduleRequests.Where(x => x.Active).ToListAsync();
         }
 
         public bool DoesRequestExistsForBooking(int userId, int bookingId)
@@ -64,7 +76,29 @@
 
             try
             {
-                this._bevakningContext.Entry(request).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                //this._bevakningContext.Entry(request).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                this._bevakningContext.ScheduleRequests.Update(request);
+                await this._bevakningContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> RemoveBookingRequest(DataAccessLibrary.Models.ScheduleRequests request)
+        {
+            if (this._bevakningContext == null || this._bevakningContext.ScheduleRequests == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                this._bevakningContext.Remove(request);
+                await this._bevakningContext.SaveChangesAsync();
 
                 return true;
             }
