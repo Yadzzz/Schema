e5e8f8ee231b145a16cc67b38f6a219f261a2ed4
Yad <yadmarzan@hotmail.com>
2023-08-27 21:32:10 +0200

    987987897897

--- Schema/Services/EmailService.cs
+++ Schema/Services/EmailService.cs
@@ -51,5 +51,44 @@ namespace Schema.Services
                 Console.WriteLine("Error sending email: " + ex.Message);
             }
         }
+
+        public async Task SendBookingRequestEmail(string text)
+        {
+            try
+            {
+                //SMTP settings
+                string smtpServer = "send.one.com";
+                int smtpPort = 587; // The port number may vary depending on your email provider
+                string smtpUsername = "no-reply@vastkustbevakning.se";
+                string smtpPassword = "vast123vast";
+
+                // Email information
+                string fromAddress = "no-reply@vastkustbevakning.se";
+                string toAddress = "planering@vastkustbevakning.se";
+                string subject = "Bokningsförfrågan";
+                string body = text;
+
+                // Create a new MailMessage
+                using (MailMessage message = new MailMessage(fromAddress, toAddress, subject, body))
+                {
+                    // Create an instance of SmtpClient
+                    using (SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort))
+                    {
+                        smtpClient.UseDefaultCredentials = false;
+                        smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                        smtpClient.EnableSsl = true;
+
+                        // Send the email
+                        smtpClient.Send(message);
+                    }
+                }
+
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending email: " + ex.Message);
+            }
+        }
     }
 }
